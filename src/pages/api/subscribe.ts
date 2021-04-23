f0180884befc79013612661f8847720763eaaa60
Diogo <dnalves52@gmail.com>
2021-04-23 17:11:33 -0300

    Ouvindo webhooks

--- src/pages/api/subscribe.ts
+++ src/pages/api/subscribe.ts
@@ -54,7 +54,7 @@ export default async (req: NextApiRequest, res: NextApiResponse) => {
       payment_method_types: ['card'],
       billing_address_collection: 'required',
       line_items: [
-        { price: 'price_1Ii4W6Ko46OvnYmLblYjLd0A', quantity: 1 }
+        { price: 'price_1IjDFDKo46OvnYmLyyVdEDKh', quantity: 1 }
       ],
       mode: 'subscription',
       allow_promotion_codes: true,
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -39,7 +39,7 @@ export default function Home({ product }: HomeProps) {
 }
 
 export const getStaticProps: GetStaticProps = async () => {
-  const price = await stripe.prices.retrieve('price_1Ii4W6Ko46OvnYmLblYjLd0A')
+  const price = await stripe.prices.retrieve('price_1IjDFDKo46OvnYmLyyVdEDKh')
 
   const product = {
     priceId: price.id,
